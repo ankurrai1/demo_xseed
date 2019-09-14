1f3b741a653396899cb0c13bb1fdaab719f2b079
ankur <rai.ankur0007@gmail.com>
2019-09-14 19:31:49 +0530

    Ankur | added gmae model to project

---
+++ backend/models/game.js
@@ -0,0 +1,15 @@
+const mongoose =require("mongoose");
+
+const GameShema = new mongoose.Schema({
+    gamestatus:{
+        type:JSON,
+    }},
+{ timestamps: true });
+
+let Game
+try {
+    Game = mongoose.model('Game')
+} catch (error) {
+    Game = mongoose.model('Game', GameShema)
+}
+module.exports = Game;
