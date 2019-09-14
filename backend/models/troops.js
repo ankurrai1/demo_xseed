28cb8f83412e0609da669fe86864ecbc9ac2962c
ankur <rai.ankur0007@gmail.com>
2019-09-14 17:15:25 +0530

    added troopes schema to project

---
+++ backend/models/troops.js
@@ -0,0 +1,35 @@
+const mongoose =require("mongoose");
+
+const TroopsSchema = new mongoose.Schema({
+    Kind: {
+        type: String,
+        required: [true, "field required"],
+    },
+    Description:{
+        type:String,
+    },
+    Strength: {
+        type: String,
+    },
+    Agility:{
+        type:String,
+    },
+    Intelligence:{
+        type:String,
+    },
+    Terrain:{
+        type:String,
+    },
+    Type:{
+        type:String,
+    }
+},
+{ timestamps: true });
+
+let Troops
+try {
+    Troops = mongoose.model('Troops')
+} catch (error) {
+    Troops = mongoose.model('Troops', TroopsSchema)
+}
+module.exports = Troops;
