2f737f04fa36f599e4ca509b65aa510db85e8540
ankur <rai.ankur0007@gmail.com>
2019-09-15 19:19:04 +0530

    Ankur| added request handlers to game

---
+++ backend/handlers/game.js
@@ -0,0 +1,54 @@
+const Game = require("../models/game")
+
+exports.create = (req, res) => {
+    data = req.body;
+    const user = new User(validData);
+    try {
+        const save= user.save();
+        return res.status(200).json({ token, save, message: "game created" });
+    } catch (errors) {
+        return res.status(500).json({ errors: errors.mapped() });
+    }
+};
+
+exports.all = (req, res)=> {
+    try {
+        const games = await Game.find({});
+        return res.status(200).json({ games });
+    } catch (errors) {
+        return res.status(500).json({ errors });
+    }
+};
+
+
+exports.byId = (req, res) => {
+    validData = req.body;
+    try {
+        const game = Game.findById(validData.id);
+        if (!game) return res.status(404).json({ errors: "game not found" });
+        return res.status(200).json({ game });
+    } catch (errors) {
+        return res.status(500).json({ errors });
+    }
+};
+
+exports.update = (req, res)=> {
+    validData = req.body;
+    try {
+        Game.findByIdAndUpdate(validData.id, validData.data);
+        const game = await game.findById(validData.id);
+        return res.status(200).json({ game, message: "game has been updated" });
+    } catch (errors) {
+        return res.status(500).json({ errors });
+    }
+};
+
+exports.remove =  (req, res)=> {
+    validData = req.body;
+    try {
+        const game = await Game.findByIdAndRemove(validData.id);
+        return res.status(200).json({ game, message: "game has been removed" });
+    } catch (errors) {
+        return res.status(500).json({ errors });
+    }
+};
