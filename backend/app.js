5cfe0152dc35e7e241dc347afa094f8a7b752b24
ankur <rai.ankur0007@gmail.com>
2019-09-20 19:15:07 +0530

    removed mongo configuration

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,42 @@
 const logger = require("morgan"); // to log user requested url on server
 const dotenv = require("dotenv"); // to config envirnment
 
-const bluebird = require("bluebird"); // used for creating promise
+// const bluebird = require("bluebird"); // used for creating promise
 const express = require("express");
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const route = require('./routes')
 const cors = require("cors");
 
 const session = require('express-session');
-const MongoStore = require("connect-mongo")(session)
+// const MongoStore = require("connect-mongo")(session)
 const app = express();
 dotenv.config({path: ".env"});
 
 let port = process.env.PORT ;
-let mongoUrl = process.env.MONGO_URI;
+// let mongoUrl = process.env.MONGO_URI;
 
 app.set("port", port);
 
-(mongoose).Promise = bluebird;
-mongoose.connect(mongoUrl,{ useNewUrlParser: true })
-        .then(() => console.log("Connected to Mongo DB"))
-.catch(err =>console.log("MongoDB connection error. Please make sure MongoDB is running. " + err));
+// (mongoose).Promise = bluebird;
+// mongoose.connect(mongoUrl,{ useNewUrlParser: true })
+//         .then(() => console.log("Connected to Mongo DB"))
+// .catch(err =>console.log("MongoDB connection error. Please make sure MongoDB is running. " + err));
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: process.env.SESSION_SECRET,
-    store: new MongoStore({
-        url: mongoUrl,
-        autoReconnect: true
-    })
-}));
+// app.use(session({
+//     resave: true,
+//     saveUninitialized: true,
+//     secret: process.env.SESSION_SECRET,
+//     store: new MongoStore({
+//         url: mongoUrl,
+//         autoReconnect: true
+//     })
+// }));
 
 app.use(logger('dev'));
 app.get('/all',route.getAlltroops)
